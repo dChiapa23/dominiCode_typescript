2260e1216b9d07a96d374718c862b9dbda18dc61
Diego Chiapa <dchiapa@frba.utn.edu.ar>
2022-12-11 22:03:03 -0300

    definir tipos de datos

---
+++ 01_TSC/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+// Typescript es una version de JS con tipado Estatico
+// Podemos asignarle tipos especificos a cada variable
+// Para no cometer errores al cambiar el tipo durante la creacion del proyecto
+/* TIPOS DE DATOS
+
+JS:
+
+- strings
+- number
+- null
+- undefined
+- boolean
+- bigInt
+- object
+- function
+- symbol
+
+TS
+
+- any
+- arrays
+- tuplas
+- enum
+- never
+
+*/
+let userName = "Diego";
+let userAge = 30;
+console.log(`Hola mundo mi nombre es: ${userName} y  tengo ${userAge} años`);
---
+++ 01_TSC/src/index.ts
@@ -0,0 +1,32 @@
+// Typescript es una version de JS con tipado Estatico
+// Podemos asignarle tipos especificos a cada variable
+// Para no cometer errores al cambiar el tipo durante la creacion del proyecto
+
+/* TIPOS DE DATOS
+
+JS:
+
+- strings
+- number
+- null
+- undefined
+- boolean
+- bigInt
+- object
+- function
+- symbol
+
+TS
+
+- any
+- arrays
+- tuplas
+- enum
+- never
+
+*/
+
+let userName: string = "Diego";
+let userAge: number = 30;
+
+console.log(`Hola mundo mi nombre es: ${userName} y  tengo ${userAge} años`);
